e77f353875795e367ca85237bde5fd5bd79d1aad
zhenya457 <derkachzhenya>
2023-01-07 21:38:25 +0200

    burger1

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let burger = document.getElementById('burger');
+
+    burger.addEventListener('click', function () {
+        burger.classList.toggle('open');
+    })
+
+});
